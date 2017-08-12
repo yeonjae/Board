5d78f62a0fa6c22d63e0a7199f7fc417f24d9856
yeonjae <hxw@daum.net>
2017-08-12 19:07:03 +0900

    폴리곤
    모바일에서 원활하게 구동할 수 있도록 폴리곤을 줄여봤습니다.
    1학기동안 구현한 기능은 모두 동작하면서, 필요없는 부분은 모두 지웠습니다.
    그러므로 2학기부터는 이 프로젝트를 바탕으로 작업하시면 됩니다.
    폴리곤 관련해서 발표자료도 만들었습니다. 다만, 2페이지에 폴리곤에 대한 설명을 써야합니다.

---
+++ VR Snowboard 2.0/Assets/Scripts/Gravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour
+{
+
+    public GUIText _test; // 디버그를 위한 GUI TEXT
+    public GameObject _target; // 가속도 센서를 적용할 객체, 여기서는 판
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 가속도 센서 디버그, 게임에 영향없음
+        _test.text = Input.acceleration.ToString();
+
+        // 객체의 각도 값을, 스마트폰의 각도와 동일시함
+        _target.transform.localEulerAngles =
+            new Vector3(Input.acceleration.y * 90.0f, Input.acceleration.z * 90.0f, Input.acceleration.x * -90.0f);
+    }
+}
